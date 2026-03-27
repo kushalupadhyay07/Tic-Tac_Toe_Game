de3c9668d4114ce012a30a0edafec68efb7bfdb8
Kushal upadhyay <kushalupadhyay1818@gmail.com>
2026-03-27 18:08:07 +0530

    update: modified existing functionality

--- src/Components/Play.jsx
+++ src/Components/Play.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Play() {
-  let items=[" "," "," "," "," "," "," "," "," ",];
+  const [items, setItems] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [toggle, setToggle] = useState(true);
+
+  function handleClick(index) {
+    if (items[index] === "") {
+      let value = toggle ? "X" : "0";
+
+      let newItems = [...items];
+      newItems.splice(index, 1, value);
+
+      setItems(newItems);
+      setToggle(!toggle);
+    }
+  }
 
   return (
-    <div className='w-25/100 border-2 h-9/10 grid grid-cols-3 gap-2 box-border p-2 bg-yellow-100'>
-      {
-        items.map((item,index)=>{
-          return <div className='border-2 cursor-pointer rounded-2xl bg-orange-700 hover:bg-yellow-500 active:bg-amber-600' >
-
+    <div className="w-25/100 border-2 h-9/10 grid grid-cols-3 gap-2 p-2 bg-yellow-100">
+      {items.map((item, index) => {
+        return (
+          <div
+            key={index}
+            onClick={() => handleClick(index)}
+            className="border-2 cursor-pointer rounded-2xl flex items-center justify-center text-3xl bg-orange-700 hover:bg-yellow-500 active:bg-amber-600 h-[80px]"
+          >
+            {item}
           </div>
-        })
-      }
-      
+        );
+      })}
     </div>
-  )
+  );
 }
